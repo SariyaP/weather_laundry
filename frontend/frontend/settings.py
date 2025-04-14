4f6eed21c14be0391dfbd49c5892d949749d840e
Sariya Punprang <sariya.pu@ku.th>
2025-04-14 16:47:30 +0700

    Add more components

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = 'static/'
 
 # Default primary key field type
